fdd6200b660b85ff9f995bc5819ae5cd1f42c28b
Vikas Z <vikas.zingade@finaccel.co>
2026-01-28 09:17:29 +0530

    Add entrypoint: main function

---
+++ rust-json-parser/src/main.rs
@@ -0,0 +1,13 @@
+use rust_json_parser::tokenizer::tokenize;
+
+fn main() {
+    let json = r#"{ "name": "Alice", "age": 30 }"#;
+    println!("Input JSON: {}", json);
+
+    let tokens = tokenize(json);
+
+    println!("\nTokens:");
+    for token in &tokens {
+        println!("{:?}", token);
+    }
+}
